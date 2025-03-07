5b38a702c9a8692d85517f4ff724514d27e575b3
Sebastian <sebberrios99@gmail.com>
2025-03-06 19:57:56 -0500

    fix: routes on vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import PokemonProvider from "./contexts/PokemonContext";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -7,29 +7,16 @@ import Layout from "./components/Layout";
 import NotFound from "./pages/NotFound";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "pokemon/:pokemonID",
-        element: <Detail />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <PokemonProvider>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="pokemon/:pokemonID" element={<Detail />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </PokemonProvider>
 );
